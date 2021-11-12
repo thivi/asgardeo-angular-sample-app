58cb33c90353b8857544026faeb6c5fda0053b1d
Thivi <theviyanthan@outlook.com>
2021-11-13 05:09:52 +0530

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { AsgardeoAuthService, Hooks } from '@asgardeo/auth-angular';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'asgardeo-angular-sample-app';
+  isAuthenticated = false;
+
+  public constructor(private auth: AsgardeoAuthService) {
+    auth.on(Hooks.SignIn, () => {
+      this.isAuthenticated = true;
+    });
+   }
+
+  public login() {
+    this.auth.signIn();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AsgardeoAuthModule } from '@asgardeo/auth-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AsgardeoAuthModule.forRoot({
+      signInRedirectURL: 'http://localhost:4200/',
+      signOutRedirectURL: 'http://localhost:4200/',
+      clientID: '<client-id>>',
+      serverOrigin: 'https://api.asgardeo.io/t/<tenant>',
+      scope: ['openid', 'profile'],
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
